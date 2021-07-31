c15b2229b628e28cb2edfff24567fbb0b2613785
izzat <tiran.sss@mail.ru>
2021-07-31 14:44:02 +0500

    "Commit"

--- indicator/settings.py
+++ indicator/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # new
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
--- indicator/urls.py
+++ indicator/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('dashboard.urls')),
                   path('api/', include('api.urls')),
-                  path('', include('product.urls')),
+                  # path('', include('product.urls')),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
