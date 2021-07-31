e1cd59e9aae5ae8e602612e62139dd0308de51a1
izzat <tiran.sss@mail.ru>
2021-07-31 15:14:30 +0500

    "Commit"

--- indicator/settings.py
+++ indicator/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-334tw0f9*e$$(8vwrjmvoiyzzg)_a!37(q_r9l#nqqzuax(2=7+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'users.CustomUser'  # ne
