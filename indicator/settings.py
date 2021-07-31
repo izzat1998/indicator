96ba5aff359dfc1f949c219dbcfe93544fb10014
izzat <tiran.sss@mail.ru>
2021-07-31 13:52:52 +0500

    "Commit"

--- indicator/settings.py
+++ indicator/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = 'root/indicator/static/'  # new
+STATICFILES_DIRS = 'root/indicator/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # new
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
